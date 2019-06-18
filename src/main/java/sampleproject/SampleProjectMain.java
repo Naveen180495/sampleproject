d9a53562c6a3d4e450aa64724a56d5776582b183
Naveen <naveenthetechworker@gmail.com>
2019-06-18 19:07:05 +0530

    Commit for merge check

--- src/main/java/sampleproject/SampleProjectMain.java
+++ src/main/java/sampleproject/SampleProjectMain.java
@@ -4,6 +4,7 @@ public class SampleProjectMain {
 
 	public static void main(String[] args) {
 		System.out.println("This is a sample Java File");
+		System.out.println("Added for merge check");
 	}
 
 }
