56753774fbc4387608feb94e95fae2297133822e
Naveen <naveenthetechworker@gmail.com>
2019-06-23 09:10:35 +0530

    Commit to check add all operation

--- src/main/java/sampleproject/Main.java
+++ src/main/java/sampleproject/Main.java
@@ -6,6 +6,8 @@ public class SampleProjectMain {
 		System.out.println("This is a sample Java File");
 		System.out.println("Added for merge check");
 		System.out.println("Added to check git diff");
+		System.out.println("Added to check stash operation");
+		System.out.println("Added to check git add operation");
 	}
 
 }
